04989ecf232c0af7624109ca234dad190d8bdee8
DarkMetaK <matporto03@gmail.com>
2023-01-02 16:08:59 -0300

    Templates

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'; 
+
 type GameBannerProps = {
     bannerUrl: string,
     title: string,
@@ -5,8 +7,11 @@ type GameBannerProps = {
 }
 
 export function GameBanner(props: GameBannerProps) {
+
+    const [adIsOpen, setAdIsOpen] = useState(false);
+
     return (
-    <a href="" className='relative rounded-lg overflow-hidden keen-slider__slide'>
+    <a href="" className='relative rounded-lg overflow-hidden keen-slider__slide' onClick={() => {setAdIsOpen(true)}}>
         <img src={props.bannerUrl} alt=""/>
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
